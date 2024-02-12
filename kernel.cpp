1bc757af722134d334fd6f23b5841b297b6f6605
mikhail-linux-3 <rachkov.mv@phystech.edu>
2024-02-12 20:26:36 +0300

    0.1.1 added kernel + asm + linker

---
+++ kernel.cpp
@@ -0,0 +1,17 @@
+using sizeT = unsigned long int;
+
+void putline(char* line)
+{
+    unsigned short* video_memory_entry = (unsigned short*) 0xB8000;
+
+    for (sizeT cnt = 0; line[cnt] != '\0'; cnt++)
+    {
+        video_memory_entry[cnt] = (video_memory_entry[cnt] & 0xFF00) | line[cnt]; //save high bytes (color info)
+    }
+    
+}
+
+extern "C" void kernel_main(void* multiboot_struture, unsigned int magic_number)
+{
+    putline("Hello User! ===ZOS=== Mikhail038, Dash8f\n");
+}
---
+++ loader.s
@@ -0,0 +1,28 @@
+.set MAGIC, 0x1BAD002 
+.set FLAGS, (1<<0 | 1<<1)
+.set CHEKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+.section .text
+.extern kernel_main
+.global loader
+
+loader: 
+    mov $kernel_stack, %esp
+    push %eax
+    push %ebx
+    call kernel_main
+
+_stop:
+    cli
+    halt
+    jmp _stop
+
+
+.section .bss
+.space 2*1024*1024; 2MiB
+kernel_stack:
